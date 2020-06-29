f9c575a7d4c681ac0a48fd6d134874fc1df8aaa8
toivux8 <toix82k@gmail.com>
2020-06-29 21:47:48 +0700

    Updates

---
+++ precache-manifest.6c51fd2a3f7b7652e8c067c3a4b2fb9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42d0ea239704bea9eda5e673db38eecd",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "99f3946dbaaf40cb7810",
+    "url": "/todolist/static/css/main.bc0df2cd.chunk.css"
+  },
+  {
+    "revision": "a90edf044ffa2505d4b6",
+    "url": "/todolist/static/js/2.708d1983.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist/static/js/2.708d1983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f3946dbaaf40cb7810",
+    "url": "/todolist/static/js/main.b59bf70e.chunk.js"
+  },
+  {
+    "revision": "130fd48b1b39e8c6b47c",
+    "url": "/todolist/static/js/runtime-main.5f712ceb.js"
+  }
+]);
